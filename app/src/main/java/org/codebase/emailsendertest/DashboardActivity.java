c8bf2d6ca3f311143e2395cec88487155e3afc1b
rehmanjameel <abdulrehmancs17@gmail.com>
2023-11-30 20:49:14 +0500

    sending image on email

--- app/src/main/java/org/codebase/emailsendertest/DashboardActivity.java
+++ app/src/main/java/org/codebase/emailsendertest/DashboardActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Base64;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -16,15 +20,37 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 
-import papaya.in.sendmail.SendMail;
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class DashboardActivity extends AppCompatActivity {
 
     GoogleSignInOptions signInOptions;
     GoogleSignInClient signInClient;
 
-    MaterialButton emailSend, signOut;
+    MaterialButton emailSend, signOut, generateQR;
     TextView userName, userEmail;
+    ImageView qrCodeImage;
+    String qrCodeImageBase64 = "";
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +61,13 @@ public class DashboardActivity extends AppCompatActivity {
         signOut = findViewById(R.id.signout);
         userName = findViewById(R.id.userName);
         userEmail = findViewById(R.id.userEmail);
+
+        qrCodeImage = findViewById(R.id.qrImage);
+        generateQR = findViewById(R.id.generateQrCode);
+
+        generateQR.setOnClickListener(view -> {
+            generateQRCode();
+        });
 
         // set the sign in option via email
         signInOptions = new GoogleSignInOptions
@@ -56,12 +89,16 @@ public class DashboardActivity extends AppCompatActivity {
         }
 
         emailSend.setOnClickListener(view -> {
-            SendMail mail = new SendMail("abdulrehmancs17@gmail.com", "jul2398arj@",
-                    "malikarj98@gmail.com",
-                    "Testing Email Sending",
-                    "Yes, it's working well\nI will use it always.");
-            mail.execute();
-//            new EmailSender(this).sendEmail();
+//            SendMail mail = new SendMail("abdulrehmancs17@gmail.com", "uwts nnup cjob mhap",
+//                    "malikarj98@gmail.com",
+//                    "Testing Email Sending",
+//                    "Yes, it's working well\nI will use it always."
+//            );
+//            mail.execute();
+
+            // Send email with both text and QR code image
+            EmailSender emailSender = new EmailSender(bitmap);
+            emailSender.sendEmailWithAttachment();
         });
 
         // sign out the user
@@ -70,10 +107,89 @@ public class DashboardActivity extends AppCompatActivity {
         });
     }
 
+    private void generateQRCode() {
+
+        // Create a QRGEncoder object
+        QRGEncoder qrgEncoder = new QRGEncoder("https://www.google.com", null, QRGContents.Type.TEXT, 500);
+        qrgEncoder.setColorBlack(Color.WHITE);
+        qrgEncoder.setColorWhite(Color.BLACK);
+        // Get the QR Code as Bitmap
+        bitmap = qrgEncoder.getBitmap();
+
+        // Set the Bitmap on the ImageView
+        qrCodeImage.setImageBitmap(bitmap);
+
+
+    }
+
+    private static class BitmapUtils {
+        static String bitmapToBase64(Bitmap bitmap) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            return Base64.encodeToString(byteArray, Base64.DEFAULT);
+        }
+    }
+
     private void signOutUser() {
         signInClient.signOut().addOnCompleteListener(task -> {
             startActivity(new Intent(DashboardActivity.this, MainActivity.class));
             finish();
         });
+    }
+
+    public static void sendImageEmail(Session session, String toEmail, String subject, String body){
+        try{
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            // Create the message body part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            messageBodyPart.setText(body);
+
+            // Create a multipart message for attachment
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Second part is image attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "image.png";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            //Trick is to add the content-id header here
+            messageBodyPart.setHeader("Content-ID", "image_id");
+            multipart.addBodyPart(messageBodyPart);
+
+            //third part for displaying image in the email body
+            messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent("<h1>Attached Image</h1>" +
+                    "<img src='cid:image_id'>", "text/html");
+            multipart.addBodyPart(messageBodyPart);
+
+            //Set the multipart message to the email message
+            msg.setContent(multipart);
+
+            // Send message
+            Transport.send(msg);
+            System.out.println("EMail Sent Successfully with image!!");
+        }catch (MessagingException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/org/codebase/emailsendertest/EmailSender.java
+++ app/src/main/java/org/codebase/emailsendertest/EmailSender.java
@@ -1,41 +1,98 @@
-//package org.codebase.emailsendertest;
-//
-//import android.content.Context;
-//import android.os.Handler;
-//import android.os.Looper;
-//import android.widget.Toast;
-//
-//import java.util.Properties;
-//import java.util.concurrent.Executor;
-//import java.util.concurrent.Executors;
-//
-//import javax.mail.Authenticator;
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.PasswordAuthentication;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
-//
-//public class EmailSender {
-//
-//    private final Context context;
-//    private final String username = "abdulrehmancs17@gmail.com";
-//    private final String password = "jul2398Arj@";
-//    private final String to = "malikarj98@gmail.com";
-//
-//    public EmailSender(Context context) {
-//        this.context = context;
-//    }
-//
-//    public void sendEmail() {
-//        // Use Executors.newSingleThreadExecutor() to run the task in a background thread
-//        Executor executor = Executors.newSingleThreadExecutor();
-//
-//        executor.execute(new EmailSenderRunnable());
-//    }
-//
+package org.codebase.emailsendertest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class EmailSender {
+
+    private final String username = "abdulrehmancs17@gmail.com";
+    private final String password = "uwts nnup cjob mhap";
+    private final String to = "malikarj98@gmail.com";
+    private static final String SUBJECT = "Testing Email Sending";
+    private static final String MESSAGE = "Yes, it's working well. I will use it always.";
+
+    private Bitmap imageAttachment;
+
+    public EmailSender(Bitmap imageAttachment) {
+        this.imageAttachment = imageAttachment;
+    }
+
+    public void sendEmailWithAttachment() {
+        Executor executor = Executors.newSingleThreadExecutor();
+        // Your existing email sending logic
+        executor.execute(this::sendEmail);
+    }
+
+    private void sendEmail() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(SUBJECT);
+
+            // Create the message body
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText(MESSAGE);
+
+            // Create the attachment
+            MimeBodyPart imageBodyPart = new MimeBodyPart();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            imageAttachment.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            imageBodyPart.setContent(byteArray, "image/png");
+            imageBodyPart.setFileName("qrcode.png");
+
+            // Combine message and attachment
+            MimeMultipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            multipart.addBodyPart(imageBodyPart);
+
+            message.setContent(multipart);
+
+            // Send the message
+            Transport.send(message);
+
+            Log.e("sending", "email sent");
+        } catch (MessagingException e) {
+            Log.e("sending", "email not sent" + e.toString());
+
+        }
+    }
+
 //    private class EmailSenderRunnable implements Runnable {
 //        @Override
 //        public void run() {
@@ -96,6 +153,6 @@
 //            }
 //        });
 //    }
-//}
-//
-//
+}
+
+
